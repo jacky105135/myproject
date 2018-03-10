18d744f7a7e18201201741f5ae81bff8e450012d
jacky105135 <jacky105135@gmail.com>
2018-03-11 00:56:36 +0800

    Cars add

--- src/com/java2/object/Cars.java
+++ src/com/java2/object/Cars.java
@@ -1,5 +1,35 @@
 package com.java2.object;
 
 public class Cars {
+	String brand;//品牌
+	String color;//顏色
+	int speed;//速度
 
+	public Cars( String a , String b){
+		brand = a;
+		color = b;
+		speed = 0;
+	}
+	
+	public void accelerate(){
+		int aa = 10;//加速度
+		int t = 0;
+		if(speed < 200){
+			t++;
+			speed = speed + aa * t;
+		}else{
+			System.out.println("The speed is at the limit");
+		}
+	}
+	
+	public void SpeedDown(){
+		int bb = -10;
+		int t = 0;
+		if(speed == 0){
+			t++;
+			speed = speed + bb * t;
+		}else{
+			System.out.println("The car is stop already");
+		}
+	}
 }
