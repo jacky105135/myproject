d069bc2beacb0e5d5b9872e2912a600dd3b03db4
jacky105135 <jacky105135@gmail.com>
2018-06-09 23:18:48 +0800

    player

--- src/com/java2/oo/PlayMain.java
+++ src/com/java2/oo/PlayMain.java
@@ -3,7 +3,7 @@ package com.java2.oo;
 public class PlayMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/com/java2/oo/Player.java
+++ src/com/java2/oo/Player.java
@@ -1,5 +1,29 @@
 package com.java2.oo;
 
 public class Player {
+	int HP;
+	String Name;
+	
+	public Player(int hP, String name) {
+		super();
+		HP = hP;
+		Name = name;
+	}
 
+	public int getHP() {
+		return HP;
+	}
+
+	public void setHP(int hP) {
+		HP = hP;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+	
 }
