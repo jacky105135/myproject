83d53af8738060aa4f6d85d864dc3a0281889c09
jacky105135 <jacky105135@gmail.com>
2018-06-24 16:39:07 +0800

    maze bug eli

--- src/com/java2/oo/PlayGround.java
+++ src/com/java2/oo/PlayGround.java
@@ -5,19 +5,28 @@ import java.util.List;
 import java.util.Scanner;
 
 public class PlayGround {
-	
+
 	List<Player> playerlist = new ArrayList<>();
+
 	public void addPlayer(Player player) {
 		playerlist.add(player);
 	}
-	
+
 	public void on() {
 		System.out.println("請輸入玩家姓名:");
 		Scanner scanner = new Scanner(System.in);
 		String name = scanner.nextLine();
-		playerlist.add(new Player(100, name, 0));
-		//System.out.println("Hello,"+ name + "!!!");
-		
+		addPlayer(new Player(100, name, 0));
+
+		// for (int i = 0; i < playerlist.size(); i++) {
+		// Player player = playerlist.get(i);
+		// System.out.println(player.getHP() + " " + player.getName() + " " +
+		// player.getPosition());
+		//
+		// }
+
+		// System.out.println("Hello,"+ name + "!!!");
+
 	}
-	
+
 }
--- src/com/java2/oo/PlayMain.java
+++ src/com/java2/oo/PlayMain.java
@@ -13,6 +13,7 @@ public class PlayMain {
 
 	public static void main(String[] args) {
 		int hp = 100;
+		int position = 0;
 		PlayGround pg = new PlayGround();
 		List<String> traps = new ArrayList<>();
 		Random r = new Random();
@@ -28,13 +29,26 @@ public class PlayMain {
 			// System.out.println(width);
 			int trap = Integer.parseInt(br.readLine());
 			// System.out.println(trap);
-			for (int k = 1; k <= trap; k++) {
-				traps.add(Integer.toString(r.nextInt(23)));
-			}
+			
+			int[] no1 = new int[trap];
+			for (int k = 0; k <= trap - 1; k++) {
+				int no = r.nextInt(23);
+				no1[k] = no; 
+				if (k == 0) {
+					traps.add(Integer.toString(no));
+					
+				}else if (no1[k] == no1[k-1]) {
+						no = no - 1;
+						traps.add(Integer.toString(no));
+					} else {
+						traps.add(Integer.toString(no));
+					}
+				}
+			
 			System.out.println(traps);
 			String route = br.readLine();
 			String[] routes = route.split(",");
-		
+
 			for (int i = 0; i < width * length; i++) {
 				if (i % 6 == 5 && i != 0 && i != 5) {
 					System.out.println(i);
@@ -46,53 +60,72 @@ public class PlayMain {
 					System.out.print(i + " ");
 				}
 			}
-				
+
 			pg.on();
-			
+
+			int step = 1;
 			int steps = 0;
-			
+
+			System.out.print(routes[0] + " → ");
 			for (int f = 1; f < routes.length; f++) {
-				if(routes[f].equals(traps.get(0))||routes[f].equals(traps.get(1))||routes[f].equals(traps.get(2))||routes[f].equals(traps.get(3))||routes[f].equals(traps.get(4))){	
-					hp = hp - 50;
-					steps = steps + 1;
-					//System.out.println(hp);
-					
-			}else if (routes[f] != routes[f - 1]) {
-					steps = steps + 1;
-					//System.out.println(hp);
+				if (routes[f].equals(traps.get(0)) || routes[f].equals(traps.get(1)) || routes[f].equals(traps.get(2))
+						|| routes[f].equals(traps.get(3)) || routes[f].equals(traps.get(4))) {
+					hp = hp - 20 - step;
+					steps++;
+					if (hp > 0) {
+						if (f == 14) {
+							System.out.print(routes[f]);
+							System.out.println();
+						} else {
+							System.out.print(routes[f] + " → ");
+						}
+					} else {
+						System.out.print(routes[f]);
+						System.out.println();
+						break;
+					}
+				} else if (routes[f] != routes[f - 1]) {
+					hp = hp - step;
+					steps++;
+					if (hp > 0) {
+						if (f == 14) {
+							System.out.print(routes[f]);
+							System.out.println();
+						} else {
+							System.out.print(routes[f] + " → ");
+						}
+					} else {
+						System.out.print(routes[f]);
+						System.out.println();
+						break;
+					}
 				}
 			}
-			
-			route = route.replaceAll(",", " → ");
-			System.out.println(route);
-			hp = hp - steps;
+
+			// hp = hp - steps;
 
 			String A = null;
-			
-			if(hp <= 0){
+
+			if (hp <= 0) {
 				A = "死亡";
-			}else if(hp >= 70){
+			} else if (hp >= 70) {
 				A = "良好";
-			}else if(hp >= 30){
+			} else if (hp >= 30) {
 				A = "普通";
-			}else{
+			} else {
 				A = "瀕臨死亡";
 			}
-			
-			if(hp <= 0){
+			// System.out.println(steps);
+			if (hp <= 0) {
 				System.out.println("hp left : 0");
-				}else{
-					System.out.println("hp left : "+hp);
-				}
-			
-			if(hp <= 0){
-			System.out.println("final position : 已死亡" );
-			}else{
-				System.out.println("final position : "+ routes[steps]);
+				System.out.println("final position :" + routes[steps]);
+			} else {
+				System.out.println("hp left : " + hp);
+				System.out.println("final position : " + routes[steps]);
 			}
-			System.out.println("status : "+ A);
-			
-			
+
+			System.out.println("status : " + A);
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
